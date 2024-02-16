74c1f90f9fa8c9dfe648d9c4689523e143fd3911
Nishat Firoj <nishat.firoj@gmail.com>
2024-02-16 15:42:32 -0500

    update website title

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -1,8 +1,14 @@
+import Tape from "@/components/tape"
+import spotifyCard from "../../../public/svg/spotifyCard.svg"
+import Image from "next/image"
+
 export default function Jupiter() {
   return (
     <>
-      <h1>Jupiter</h1>
-      <p>Jupiter goods</p>
+      <div style={{ position: "relative" }}>
+        <Tape top="-12px" right="12px" />
+        <Image src={spotifyCard} alt="Spotify card" />
+      </div>
     </>
   )
 }
