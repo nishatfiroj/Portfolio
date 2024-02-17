b18a788a4a29ddea570da758cfa2a54aadbfbda0
Nishat Firoj <nishat.firoj@gmail.com>
2024-02-16 23:33:37 -0500

    return button on construction page

--- src/app/ncr/page.tsx
+++ src/app/ncr/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link"
+import Image from "next/image"
+import goBack from "../../../public/svg/goBack.svg"
+
 export default function NCR() {
-  return <h1>Pardon my dust y&apos;all</h1>
+  return (
+    <>
+      <h1>Pardon my dust y&apos;all</h1>
+      <Link href="/">
+        <Image
+          src={goBack}
+          alt="Navigate back to home screen"
+          style={{ padding: "16px" }}
+        />
+      </Link>
+    </>
+  )
 }
